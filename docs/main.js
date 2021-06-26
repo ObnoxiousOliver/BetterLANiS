747bf3b74d1d82d1cc6048a4fe3168640c729e1c
Oliver <maxhd2005@gmail.com>
2021-06-26 17:46:17 +0200

    Docs
    Comming Soon State

---
+++ docs/main.js
@@ -0,0 +1,9 @@
+
+// GET VERSION
+// fetch('https://api.github.com/repos/ObnoxiousOliver/auto-update-test/releases')
+//   .then(res => res.json())
+//   .then(data => {
+//     const VERSION = data[0].tag_name
+
+//     document.getElementById('version').textContent = VERSION
+//   })
