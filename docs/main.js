31f18b7818b4f051f5b8f69cf326454144acab07
Oliver <maxhd2005@gmail.com>
2021-06-28 16:26:10 +0200

    Docs
    made docs zoomable

--- docs/main.js
+++ docs/main.js
@@ -1,9 +1,14 @@
-
 // GET VERSION
-// fetch('https://api.github.com/repos/ObnoxiousOliver/auto-update-test/releases')
+// fetch('https://api.github.com/repos/ObnoxiousOliver/BetterLANiS/releases', {
+//   headers: {
+//     Authorization: 'Basic ' + btoa('*:' + 'TOKEN')
+//   }
+// })
 //   .then(res => res.json())
 //   .then(data => {
-//     const VERSION = data[0].tag_name
+//     const release = data[0]
+//     const asset = release.assets.filter(x => x.name.endsWith('.exe'))[0]
 
-//     document.getElementById('version').textContent = VERSION
+//     document.querySelector('.download-btn').href = asset.browser_download_url
+//     document.getElementById('version').textContent = '(v' + release.tag_name + ')'
 //   })
