c8768c0b74777bcf28e879f121afffe7be279030
Oliver <maxhd2005@gmail.com>
2021-07-23 16:30:19 +0200

    feat(internal browser): inject css
    feat: default scss written for start.schulportal.hessen.de

--- src/views/Unsupported.vue
+++ src/views/Unsupported.vue
@@ -20,6 +20,7 @@
 import PageControls from '@/components/PageControls'
 
 import manager from '@/manager'
+import { mapState } from 'vuex'
 
 export default {
   name: 'Unsupported',
@@ -32,6 +33,9 @@ export default {
     location: undefined
   }),
   computed: {
+    ...mapState([
+      'theme'
+    ]),
     sourceUrl () {
       if (this.$route.params.link && this.setUrl) {
         return manager.urls.BASE_ADDRESS + this.$route.params.link.replaceAll('&query:', '?')
@@ -61,35 +65,59 @@ export default {
     iframeLoad (e) {
       this.pageLoading = false
 
-      var frame = e.target.contentWindow
+      const frame = e.target.contentWindow
       this.location = frame.location
 
-      // Inject CSS
-      // var style = document.createElement('style')
-      // style.innerHTML = `
-      //   * {
-      //     transition: background-color 0.2s;
-      //   }
-      //   h1, h2, h3, h4, h5, h6 {
-      //     font-family: Poppins;
-      //   }
-      //   .navbar-custom, .header-top, .ccm-page #header .header-top.header-top-colored, .sticky-wrapper, .footer, #headlogo {
-      //     display: none !important;
-      //   }
-      //   body {
-      //     overflow: auto;
-      //     font-family: Heebo;
-      //   }
-      // `
-      // frame.document.head.append(style)
+      this.injectCss(true)
+      frame.document.documentElement.setAttribute('frame-host', frame.location.host)
+      frame.document.documentElement.setAttribute('frame-path', frame.location.pathname)
 
       // A-tags open Browser
       frame.document.querySelectorAll('a').forEach(el => {
         // console.log(el.href, this.allowedLinks.filter(x => el.href.match(x)))
         if (!this.allowedLinks.filter(x => el.href.match(x)).length) {
           el.target = '_blank'
+        } else {
+          el.target = ''
         }
       })
+    },
+    injectCss (inject) {
+      try {
+        const frame = this.$refs.viewer.contentWindow
+
+        if (!inject) {
+          frame.document.querySelectorAll('bl-injected-styles').forEach(x => x.remove())
+          return
+        }
+
+        /* eslint import/no-webpack-loader-syntax: off */
+        const injectCss = require('!!raw-loader!@/assets/browser_inject/browser_inject.css').default
+
+        var rootVars = Array.from(document.styleSheets)
+          .filter(sheet => sheet.href === null || sheet.href.startsWith(window.location.origin))
+          .reduce((acc, sheet) => (
+            acc = [
+              ...acc, ...Array.from(sheet.cssRules)
+                .reduce((def, rule) => (
+                  def = rule.selectorText === ':root'
+                    ? [...def, ...Array.from(rule.style).filter(name => name.startsWith('--'))]
+                    : def),
+                [])
+            ]),
+          [])
+
+        frame.document.querySelectorAll('bl-injected-styles').forEach(x => x.remove())
+        var style = frame.document.createElement('style')
+        style.id = 'bl-injected-styles'
+        style.innerHTML = ':root {\n' +
+          rootVars
+            .map(x => `${x}: ${getComputedStyle(document.documentElement).getPropertyValue(x)};`)
+            .join('\n') +
+        '}\n\n' + injectCss
+
+        frame.document.documentElement.append(style)
+      } catch {}
     }
   }
 }
